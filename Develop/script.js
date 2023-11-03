2aa9791c32174d97c53e1dd29e20666f959063e8
Ricky Carter <randolphcarter5@gmail.com>
2023-11-02 22:01:34 -0400

    stored variables for further use

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+// variables stored 
+let rock = R;
+let paper = P;
+let scissors = S; 
+
+let total = [
+    wins = 0,
+    ties = 0,
+    losses = 0 
+]
+
+const message = "Would you like to play?";
