73240483eca771e758eccc46953362764a2adb0f
Ricky Carter <randolphcarter5@gmail.com>
2023-11-02 23:36:43 -0400

    updated script file with prompt() with conditions for user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,19 @@
-// variables stored 
-let rock = R;
-let paper = P;
-let scissors = S; 
+// variable stored as an object
 
-let total = [
-    wins = 0,
-    ties = 0,
-    losses = 0 
-]
+const total = {
+    wins: 0,
+    ties: 0,
+    losses: 0,
+};
+console.log(total);
 
-const message = "Would you like to play?";
+//prompt and alert 
+const messg = prompt("Enter R, P, S");
+if (messg === "R" || messg === "r") {
+} else if (messg === "P" || messg === "p") {
+    alert("You've entered paper")
+} else if ( messg === "S" || messg === "s") {
+    alert("You've entered scissors");
+} else {
+    alert("Wrong input, Enter R, P, or S")
+}
